0e23782ad5a4894db1716a15dceb98c73553d9b1
Dale Stammen <dalestam@microsoft.com>
2014-05-12 16:18:50 -0700

    added include.bat

---
+++ windows-phone-8.0/libjpeg/include.bat
@@ -0,0 +1,3 @@
+xcopy ".\jpeg-8b\jpeglib.h" "..\prebuilt\include\libjpeg\" /iycq
+xcopy ".\jpeg-8b\jerror.h" "..\prebuilt\include\libjpeg\" /iycq
+xcopy ".\jpeg-8b\jmorecfg.h" "..\prebuilt\include\libjpeg\" /iycq
---
+++ windows-phone-8.0/libtiff/include.bat
@@ -0,0 +1,4 @@
+xcopy ".\tiff-4.0.1\libtiff\tiff.h" "..\prebuilt\include\libtiff\" /iycq
+xcopy ".\tiff-4.0.1\libtiff\tiffio.h" "..\prebuilt\include\libtiff\" /iycq
+xcopy ".\tiff-4.0.1\libtiff\tiffvers.h" "..\prebuilt\include\libtiff\" /iycq
+
