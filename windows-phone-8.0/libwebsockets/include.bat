cf6799ed4d657a99025c2615460c4e3dd8150534
Dale Stammen <dalestam@microsoft.com>
2014-05-15 14:23:44 -0700

    nuget for libwebsockets

--- windows-phone-8.0/libwebsockets/include.bat
+++ windows-phone-8.0/libwebsockets/include.bat
@@ -1,3 +1,3 @@
-xcopy ".\lib\libwebsockets.h" "..\prebuilt\include\libwebsockets\" /iycq
-xcopy ".\lib\private-libwebsockets.h" "..\prebuilt\include\libwebsockets\" /iycq
+xcopy "libwebsockets.h" "..\prebuilt\include\libwebsockets\" /iycq
+xcopy "private-libwebsockets.h" "..\prebuilt\include\libwebsockets\" /iycq
 xcopy ".\win32port\win32helpers" "..\prebuilt\include\libwebsockets\win32helpers" /iycqs
