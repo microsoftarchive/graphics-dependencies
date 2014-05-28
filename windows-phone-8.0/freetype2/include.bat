3d615b9c194ada8204d3d68200de18ebe8155a30
Dale Stammen <dalestam@microsoft.com>
2014-05-28 11:09:27 -0700

    added license and freetype2 dir

--- windows-phone-8.0/freetype2/include.bat
+++ windows-phone-8.0/freetype2/include.bat
@@ -1,1 +1,5 @@
 xcopy ".\freetype-2.5.0.1\include" "..\prebuilt\include\freetype\" /iycqs
+xcopy ".\freetype-2.5.0.1\include" "..\prebuilt\include\freetype\freetype2\" /iycqs
+xcopy ".\freetype-2.5.0.1\docs\LICENSE.TXT" "..\prebuilt\include\freetype\" /iycq
+xcopy ".\freetype-2.5.0.1\docs\FTL.TXT" "..\prebuilt\include\freetype\" /iycq
+xcopy ".\freetype-2.5.0.1\docs\GPLv2.TXT" "..\prebuilt\include\freetype\" /iycq
