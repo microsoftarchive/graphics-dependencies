d462de0a9fd2edfece4446e8d239ceec0dbc75ce
Dale Stammen <dalestam@microsoft.com>
2015-01-14 21:10:23 -0800

    added msbuild commands

--- build_libwebsockets.bat
+++ build_libwebsockets.bat
@@ -24,3 +24,53 @@ cd ..\
 cd win32
 cmake -G"Visual Studio 12 2013" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=8.1  %ARGS%  ../../../
 cd ..\..\..\..\
+
+
+echo./*
+echo. * Check VC++ environment...
+echo. */
+echo.
+
+set FOUND_VC=0
+
+if defined VS120COMNTOOLS (
+    set VSTOOLS="%VS120COMNTOOLS%"
+    set VC_VER=120
+    set FOUND_VC=1
+) 
+
+set VSTOOLS=%VSTOOLS:"=%
+set "VSTOOLS=%VSTOOLS:\=/%"
+
+set VSVARS="%VSTOOLS%vsvars32.bat"
+
+if not defined VSVARS (
+    echo Can't find VC2013 installed!
+    goto ERROR
+)
+
+
+echo./*
+echo. * Building libwebsockets..
+echo. */
+echo.
+
+
+call %VSVARS%
+if %FOUND_VC%==1 (
+msbuild  libwebsockets\bin\WindowsPhone_8.1\win32\libwebsockets.sln /p:Configuration="MinSizeRel"
+msbuild  libwebsockets\bin\WindowsPhone_8.1\arm\libwebsockets.sln /p:Configuration="MinSizeRel"
+msbuild  libwebsockets\bin\WindowsStore_8.1\win32\libwebsockets.sln /p:Configuration="MinSizeRel"
+msbuild  libwebsockets\bin\WindowsStore_8.1\arm\libwebsockets.sln /p:Configuration="MinSizeRel"
+) else (
+    echo Script error.
+    goto ERROR
+)
+
+
+goto EOF
+
+:ERROR
+pause
+
+:EOF
